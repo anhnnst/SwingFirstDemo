4c8465e5fd30e084721cf6c3690c0cca2bb1ae3e
AnhNNST <anhnnst@yahoo.com>
2018-08-09 15:47:51 +0700

    Slide 6 Demo

--- src/edu/poly/dao/CategoryDao.java
+++ src/edu/poly/dao/CategoryDao.java
@@ -36,4 +36,37 @@ public class CategoryDao {
         }
         return list;
     }
+    public void insert(Category item) throws Exception{
+        String sql = "insert into category values(?,?)";
+        try(
+                Connection con = DatabaseUtils.openConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+            ){
+            pstmt.setString(1, item.getId());
+            pstmt.setString(2, item.getName());
+            pstmt.executeUpdate();
+        }
+    }
+    public void update(Category item) throws Exception{
+        String sql = "update category set name = ? where id = ?";
+        try(
+                Connection con = DatabaseUtils.openConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+            ){
+            pstmt.setString(2, item.getId());
+            pstmt.setString(1, item.getName());
+            pstmt.executeUpdate();
+        }
+    }
+    public void delete(String id) throws Exception{
+        String sql = "delete from category where id = ?";
+        try(
+                Connection con = DatabaseUtils.openConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+            ){
+            pstmt.setString(1, id);
+            
+            pstmt.executeUpdate();
+        }
+    }
 }
